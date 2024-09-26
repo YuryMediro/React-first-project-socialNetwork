8a0551d78d483b4b24ce7c04603bfd615f08aea4
Yury Mediro <sagn375@gmail.com>
2024-09-26 16:13:24 +0300

    components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+				лого
+			</header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+const Profile = () => {
+    return (
+			<div className='content'>
+				<div>лого</div>
+				<div>ava + desc</div>
+				<div>
+					my post
+					<div>new post</div>
+				</div>
+				<div>
+					<div>post 1</div>
+					<div>post 2</div>
+				</div>
+			</div>
+		)
+}
+
+export default Profile
