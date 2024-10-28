87f61c21c3b3c6f53e843f79ba06d9d374a84000
Yury Mediro <sagn375@gmail.com>
2024-10-28 19:27:48 +0300

    Users API

--- src/components/Users/users.jsx
+++ src/components/Users/users.jsx
@@ -1,43 +1,55 @@
 import React from 'react'
 import s from './users.module.css'
+import axios from 'axios'
+import userPhoto from '../../assets/img/user.avif'
+
 const Users = props => {
 	if (props.users.length === 0) {
-		props.setUsers([
-			{
-				id: 1,
-				photoUrl:
-					'https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg',
-				followed: false,
-				fullName: 'Yury',
-				status: 'I am a boss',
-				location: { city: 'Ivanovo', country: 'Russia' },
-			},
-			{
-				id: 2,
-				photoUrl:
-					'https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg',
-				followed: false,
-				fullName: 'Roma',
-				status: 'I am a boss too',
-				location: { city: 'Moscow', country: 'Russia' },
-			},
-			{
-				id: 3,
-				photoUrl:
-					'https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg',
-				followed: true,
-				fullName: 'Sasha',
-				status: 'I am a boss too',
-				location: { city: 'Minsk', country: 'Belarus' },
-			},
-		])
+		axios
+			.get('https://social-network.samuraijs.com/api/1.0/users')
+			.then(Response => {
+				props.setUsers(Response.data.items)
+			})
+
+		// props.setUsers([
+		// 	{
+		// 		id: 1,
+		// 		photoUrl:
+		// 			'https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg',
+		// 		followed: false,
+		// 		fullName: 'Yury',
+		// 		status: 'I am a boss',
+		// 		location: { city: 'Ivanovo', country: 'Russia' },
+		// 	},
+		// 	{
+		// 		id: 2,
+		// 		photoUrl:
+		// 			'https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg',
+		// 		followed: false,
+		// 		fullName: 'Roma',
+		// 		status: 'I am a boss too',
+		// 		location: { city: 'Moscow', country: 'Russia' },
+		// 	},
+		// 	{
+		// 		id: 3,
+		// 		photoUrl:
+		// 			'https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg',
+		// 		followed: true,
+		// 		fullName: 'Sasha',
+		// 		status: 'I am a boss too',
+		// 		location: { city: 'Minsk', country: 'Belarus' },
+		// 	},
+		// ])
 	}
 
 	return props.users.map(u => (
 		<div key={u.id}>
 			<span>
 				<div>
-					<img className={s.userPhoto} src={u.photoUrl} />
+					<img
+						className={s.userPhoto}
+						src={u.photos.small != null ? u.photos.small : userPhoto}
+					/>
 				</div>
 			</span>
 			<span>
@@ -63,12 +75,12 @@ const Users = props => {
 			</span>
 			<span>
 				<span>
-					<div>{u.fullName}</div>
+					<div>{u.name}</div>
 					<div>{u.status}</div>
 				</span>
 				<span>
-					<div>{u.location.country}</div>
-					<div>{u.location.city}</div>
+					<div>{'u.location.country'}</div>
+					<div>{'u.location.city'}</div>
 				</span>
 			</span>
 		</div>
