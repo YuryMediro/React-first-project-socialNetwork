24e95438c67296da41dd9a9d8c2368a16949852a
Yury Mediro <sagn375@gmail.com>
2024-10-30 18:03:40 +0300

    extends React.Component, life cycle componentDidMount

--- src/components/Users/users.jsx
+++ src/components/Users/users.jsx
@@ -3,88 +3,64 @@ import s from './users.module.css'
 import axios from 'axios'
 import userPhoto from '../../assets/img/user.avif'
 
-const Users = props => {
-	if (props.users.length === 0) {
+class Users extends React.Component {
+	componentDidMount() {
 		axios
 			.get('https://social-network.samuraijs.com/api/1.0/users')
 			.then(Response => {
-				props.setUsers(Response.data.items)
+				this.props.setUsers(Response.data.items)
 			})
-
-		// props.setUsers([
-		// 	{
-		// 		id: 1,
-		// 		photoUrl:
-		// 			'https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg',
-		// 		followed: false,
-		// 		fullName: 'Yury',
-		// 		status: 'I am a boss',
-		// 		location: { city: 'Ivanovo', country: 'Russia' },
-		// 	},
-		// 	{
-		// 		id: 2,
-		// 		photoUrl:
-		// 			'https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg',
-		// 		followed: false,
-		// 		fullName: 'Roma',
-		// 		status: 'I am a boss too',
-		// 		location: { city: 'Moscow', country: 'Russia' },
-		// 	},
-		// 	{
-		// 		id: 3,
-		// 		photoUrl:
-		// 			'https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg',
-		// 		followed: true,
-		// 		fullName: 'Sasha',
-		// 		status: 'I am a boss too',
-		// 		location: { city: 'Minsk', country: 'Belarus' },
-		// 	},
-		// ])
 	}
 
-	return props.users.map(u => (
-		<div key={u.id}>
-			<span>
-				<div>
-					<img
-						className={s.userPhoto}
-						src={u.photos.small != null ? u.photos.small : userPhoto}
-					/>
-				</div>
-			</span>
-			<span>
-				<div>
-					{u.followed ? (
-						<button
-							onClick={() => {
-								props.unfollow(u.id)
-							}}
-						>
-							Unfollow
-						</button>
-					) : (
-						<button
-							onClick={() => {
-								props.follow(u.id)
-							}}
-						>
-							Follow
-						</button>
-					)}
-				</div>
-			</span>
-			<span>
-				<span>
-					<div>{u.name}</div>
-					<div>{u.status}</div>
-				</span>
-				<span>
-					<div>{'u.location.country'}</div>
-					<div>{'u.location.city'}</div>
-				</span>
-			</span>
-		</div>
-	))
+	render() {
+		return (
+			<div>
+				{this.props.users.map(u => (
+					<div key={u.id}>
+						<span>
+							<div>
+								<img
+									className={s.userPhoto}
+									src={u.photos.small != null ? u.photos.small : userPhoto}
+								/>
+							</div>
+						</span>
+						<span>
+							<div>
+								{u.followed ? (
+									<button
+										onClick={() => {
+											this.props.unfollow(u.id)
+										}}
+									>
+										Unfollow
+									</button>
+								) : (
+									<button
+										onClick={() => {
+											this.props.follow(u.id)
+										}}
+									>
+										Follow
+									</button>
+								)}
+							</div>
+						</span>
+						<span>
+							<span>
+								<div>{u.name}</div>
+								<div>{u.status}</div>
+							</span>
+							<span>
+								<div>{'u.location.country'}</div>
+								<div>{'u.location.city'}</div>
+							</span>
+						</span>
+					</div>
+				))}
+			</div>
+		)
+	}
 }
 
 export default Users
